b244685f9a04f9db715338e7222d4965fb080404
michellemtchai <michelle.chai@mail.utoronto.ca>
2021-08-10 16:36:58 -0400

    create ReadReceipt component

--- client/src/components/ActiveChat/Messages.js
+++ client/src/components/ActiveChat/Messages.js
@@ -1,24 +1,39 @@
 import React from "react";
 import { Box } from "@material-ui/core";
-import { SenderBubble, OtherUserBubble } from "../ActiveChat";
+import { SenderBubble, OtherUserBubble, ReadReceipt } from "../ActiveChat";
 import moment from "moment";
 
 const Messages = (props) => {
-  const { messages, otherUser, userId } = props;
+  const { messages, otherUser, userId, readIndex } = props;
   return (
     <Box>
-      {messages.map((message) => {
+      {messages.map((message, index) => {
         const time = moment(message.createdAt).format("h:mm");
 
-        return message.senderId === userId ? (
-          <SenderBubble key={message.id} text={message.text} time={time} />
-        ) : (
-          <OtherUserBubble
-            key={message.id}
-            text={message.text}
-            time={time}
-            otherUser={otherUser}
-          />
+        return (
+          <>
+            {message.senderId === userId ? (
+              <>
+                <SenderBubble
+                  key={message.id}
+                  text={message.text}
+                  time={time}
+                />
+              </>
+            ) : (
+              <OtherUserBubble
+                key={message.id}
+                text={message.text}
+                time={time}
+                otherUser={otherUser}
+              />
+            )}
+            <ReadReceipt
+              show={readIndex === index}
+              username={otherUser.username}
+              photoUrl={otherUser.photoUrl}
+            />
+          </>
         );
       })}
     </Box>
